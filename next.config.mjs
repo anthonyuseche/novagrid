92745fcbbbf281a67dc403dbe9348e8cb7e2595b
Anthony José Useche Ruiz <58037181+anthonyuseche@users.noreply.github.com>
2025-04-26 16:09:26 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
-    unoptimized: true, // Necesario para exportación estática
+    unoptimized: true,
   },
-  // Opcional: si quieres que las URLs terminen con / y generen index.html
-  // trailingSlash: true,
-}
-
-// next.config.mjs
-export default nextConfig; // ✅ Correcto (sin "=" después de default)
+};
+export default nextConfig;
